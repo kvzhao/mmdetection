1a49bfa6e1670a99e17d5c331acad3a859accd73
KV Zhao <CLT\kv_zhao@rdai-dl-dt1.clt.gocyberlink.com>
2020-03-03 11:20:20 +0800

    Add backebone: mobilenet v2

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,5 +2,12 @@ from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .mobilenet_v2 import MobileNetV2
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['make_res_layer',
+           'ResNet',
+           'ResNeXt',
+           'SSDVGG',
+           'HRNet',
+           'MobileNetV2',
+           ]
