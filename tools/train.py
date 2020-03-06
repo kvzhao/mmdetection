f06f3db0d1743a871d0d3899ac30fef62ab8c8c2
KV Zhao <CLT\kv_zhao@rdai-dl-dt2.clt.gocyberlink.com>
2020-03-06 15:03:46 +0800

    import error

--- tools/train.py
+++ tools/train.py
@@ -9,6 +9,7 @@ import mmcv
 import torch
 from mmcv import Config
 from mmcv.runner import init_dist
+#from mmcv.runner.dist_utils import init_dist
 
 from mmdet import __version__
 from mmdet.apis import set_random_seed, train_detector
